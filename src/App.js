217662014ed4035bd5803f13de276e111b320217
Akshaya <akshayasatheesh@hotmail.co.uk>
2020-09-29 14:13:12 +0100

    Created basic form for page 1 Co-authored-by: Terrence Chung <netceer112@gmail.com>

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@
 
 import React from "react";
 import "./App.css";
+import AddMedication  from "./addMedication/form"
 
 function App() {
-	return <div className="App"></div>;
+	return <div className="App">
+		<AddMedication/>
+	</div>;
 }
 
 export default App;
--- src/addMedication/form.js
+++ src/addMedication/form.js
@@ -1,21 +1,51 @@
-import React from "react"
+/** @format */
+
+import React from "react";
+import styled from "styled-components"
+
+const Form = styled.form`
+display: flex;
+flex-direction: column
+`
 
 function AddMedication() {
-    // Add states here
+	// Add states here
 
-    // The markup for the Step 1 UI
-    return(
-        <div className="form-group">
-        <label htmlFor="email">Email address</label>
-        <input
-            className="form-control"
-            id="email"
-            name="email"
-            type="text"
-            placeholder="Enter email"
-            value={this.props.email} // Prop: The email input data
-            onChange={this.props.handleChange} // Prop: Puts data into state
-        />
-        </div>
-        )
+	// The markup for the Step 1 UI
+	return (
+		<Form onSubmit="">
+			<h1>Add your medicine</h1>
+
+			<label>
+				Name of medicine
+				<input name="text" type="text" value="" required />
+			</label>
+
+			<label>Type:</label>
+			<input type="checkbox" id="tablet" name="tablet" value="" />
+			<label for="tablet">Tablet</label>
+
+			<input type="checkbox" id="liquid" name="liquid" value="" />
+			<label for="liquid">Liquid</label>
+            
+			<input type="checkbox" id="needle" name="needle" value="" />
+			<label for="needle">Needle</label>
+
+			<label>
+				Strength:
+                <input name="text" type="text" value="" required />
+			</label>
+
+			<label for="unit">Unit:</label>
+            <select name="unit" id="unit">
+            <option value="mg">mg</option>
+            <option value="ml">ml</option>
+            <option value="g">g</option>
+            </select>
+
+			<button>Submit</button>
+		</Form>
+	);
 }
+
+export default AddMedication;
