acb1fbbc1d0ce77028767ca7d72aec41958795db
Akshaya <akshayasatheesh@hotmail.co.uk>
2020-09-29 16:50:40 +0100

    fix: fixed eslint but and now added CI test sample

---
+++ src/App.test.js
@@ -0,0 +1,14 @@
+/** @format */
+
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "App.js";
+
+test("Jest is working", () => {
+	expect(true).toBeTruthy();
+});
+
+test("Testing Add Medication Form loads", () => {
+  render(<App></App>);
+  screen.getByText("Add your medicine")
+}); /** @format */
