85105f23e68ee9f2d120e125ef787ae3200caa66
mhtien <59407030+mhtien@users.noreply.github.com>
2020-10-08 13:25:16 +0200

    feat(functions): fetch from nhs api
    Co-authored-by: Terrence Chung <netceer112@gmail.com>

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,57 @@
 /** @format */
 
 const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+const fetch = require("node-fetch");
+// handling errors?
+const nhsApiKey = functions.config().nhs.api_key;
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const url = "https://api.nhs.uk/medicines/";
+const param = "paracetamol-for-adults";
+const app = express();
+app.use(express.json());
+// const corsOptions = {
+// 	origin: "https://travel-jar.netlify.app/",
+// 	optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+// };
+app.use(cors());
+
+// function to fetch data from NHS API
+const getNHSData = (url, param) => {
+	return fetch(url + param, {
+		method: "GET",
+		headers: {
+			"subscription-key": nhsApiKey,
+		},
+	})
+		.then(response => {
+			if (!response.ok) {
+				const error = new Error("api error");
+				error.status = response.status;
+				throw error;
+			} else {
+				return response.json();
+			}
+		})
+		.catch(error => {
+			console.error(error);
+		});
+};
+
+app.get("/search", (req, res, next) => {
+	return getNHSData(url, param)
+		.then(data => res.send(data))
+		.catch(error => next(error));
+});
+
+app.get("/search/:medicine", (req, res, next) => {
+	return getNHSData(url, param)
+		.then(data => res.send(data))
+		.catch(error => next(error));
+});
+
+exports.nhsData = functions.https.onRequest(app);
